b838767667d423b9bebc5203ae04d367cf61fd08
İPEK DURAL <126293175+ipekdural@users.noreply.github.com>
2023-09-20 02:32:13 +0300

    Update highest_score.py

--- highest_score.py
+++ highest_score.py
@@ -1,23 +1,25 @@
 from turtle import Turtle
-scores=[]
+scores=[0]
 class HighestScore(Turtle):
     def __init__(self):
         super().__init__()
-
-
+        self.highest = self.highest_score()
     def append(self,score):
         scores.append(score)
     def highest_score(self):
         return max(scores)
     def update_scoreboard(self):
-        highest=self.highest_score()
+        with open("data.txt") as file:
+            self.highest = file.read()
         self.color("gREEN")
         self.penup()
         self.hideturtle()
         self.goto(50, 260)
-        self.write(f"Highest Score:{highest}",align="center",font=("Courier",16,"normal"))
+        self.write(f"Highest Score:{self.highest}",align="center",font=("Courier",16,"normal"))
 
 
     def write_(self):
         self.clear()
         self.update_scoreboard()
+
+
